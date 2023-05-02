20e8bfba3b1a96e8f17bf4b6565001446f1e9293
Miguel-Carreon13 <emilio.carreon@essilorusa.com>
2023-05-02 10:40:21 -0600

    lux map script added

---
+++ asn_lux_map.bat
@@ -0,0 +1,8 @@
+:: To encode Bat FFFE0A0D
+
+net use L: /delete >nul 2>nul
+net use K: /delete >nul 2>nul
+
+net use L: \\172.21.66.195\d$\Luxotica Bo$jack$0035 /USER:essilor-mx\rvenzor /persistent:no
+
+net use K: \\mx-fileseco\ECO1\Lux Bo$jack$0035 /USER:essilor-mx\rvenzor /persistent:no
