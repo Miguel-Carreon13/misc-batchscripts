5de5a5d35b8ad73f762e72ed113f2dae66ebb863
Miguel-Carreon13 <emilio.carreon@essilorusa.com>
2024-10-24 12:31:19 -0600

    repaired script

--- conteo_eco1.bat
+++ conteo_eco1.bat
@@ -2,5 +2,5 @@
 net use Q: /delete >nul 2>nul
 net use R: /delete >nul 2>nul
 
-net use Q: "\\mx-fileseco\eco1\Logistica\Inventarios\Inventario Físico ECO I\2024\1er conteo - Descarga de archivos" /persistent:no
-net use R: "\\10.153.68.53\eco1\Logistica\Inventarios\Inventario Físico ECO I\2024\1er conteo - Descarga de archivos" /persistent:no
+net use Q: "\\mx-fileseco\ECO1\Logistica\Inventarios\Inventario Fisico ECO I\2024\1er conteo - Descarga de archivos" /persistent:no
+net use R: "\\10.153.68.53\eco1\Logistica\Inventarios\Inventario Fisico ECO I\2024\1er conteo - Descarga de archivos" /persistent:no
