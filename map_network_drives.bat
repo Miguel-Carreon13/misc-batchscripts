50410b40b353945d3db939bd568c1834d11aa6e0
Miguel-Carreon13 <emilio.carreon@essilorusa.com>
2024-11-12 09:57:16 -0600

    maps batch updated

--- map_network_drives.bat
+++ map_network_drives.bat
@@ -8,13 +8,17 @@ echo.
 echo [Map Network Drives ECO labs]
 echo _____________________________
 echo.
-echo 1. Map ECO1
+echo 1. Map Fileseco ECO1
 echo.
-echo 2. Map ECO2
+echo 2. Map Fileseco ECO2
 echo.
-echo 3. Map Public
+echo 3. Map Fileseco Public
 echo.
-echo 4. Map Vic File Server
+echo 4. Map Azure ECO1
+echo.
+echo 5. Map Azure ECO2
+echo.
+echo 6. Map itdata
 echo.
 echo 0. Exit
 echo.
@@ -26,10 +30,12 @@ if not defined choice (
 	goto error
 )
 
-if %choice%==1 goto eco1Map
-if %choice%==2 goto eco2Map
-if %choice%==3 goto publicMap
-if %choice%==4 goto vicMap
+if %choice%==1 goto eco1fileseco
+if %choice%==2 goto eco2fileseco
+if %choice%==3 goto publicfileseco
+if %choice%==4 goto eco1azure
+if %choice%==5 goto eco2azure
+if %choice%==6 goto itdata
 if %choice%==0 goto:eof
 if errorlevel==1 goto error
 
@@ -39,29 +45,53 @@ title Error
 echo.
 echo.
 echo Command not recognized
-timeout /t 2 /nobreak>nul
+timeout /t 4 /nobreak>nul
 goto index
 
-:eco1Map
+:eco1fileseco
+net use x: /delete /yes
+timeout /t 3 /nobreak>nul
 net use x: \\mx-fileseco\eco1 /persistent:yes
 color 0a
 timeout /t 1 /nobreak>nul
 goto index
 
-:eco2Map
+:eco2fileseco
+net use y: /delete /yes
+timeout /t 3 /nobreak>nul
 net use y: \\mx-fileseco\eco2 /persistent:yes
 color 0a
 timeout /t 1 /nobreak>nul
 goto index
 
-:publicMap
+:publicfileseco
+net use z: /delete /yes
+timeout /t 3 /nobreak>nul
 net use z: \\mx-fileseco\public /persistent:yes
 color 0a
 timeout /t 1 /nobreak>nul
 goto index
 
-:vicMap
-net use v: \\172.22.187.9\itdata
+:eco1azure
+net use t: /delete /yes
+timeout /t 3 /nobreak>nul
+net use t: \\10.153.68.53\eco1
+color 0a
+timeout /t 1 /nobreak>nul
+goto index
+
+:eco2azure
+net use u: /delete /yes
+timeout /t 3 /nobreak>nul
+net use u: \\10.153.68.53\eco2
+color 0a
+timeout /t 1 /nobreak>nul
+goto index
+
+:itdata
+net use v: /delete /yes
+timeout /t 3 /nobreak>nul
+net use v: \\172.22.187.9\ITDATA SistemasEc0$@ /user:WIN-GTELSG14QMO\It /persistent:yes
 color 0a
 timeout /t 1 /nobreak>nul
 goto index
