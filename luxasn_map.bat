04991c0dc44e8d566d5fe73bc4b7f6d1b03657b5
Miguel-Carreon13 <emilio.carreon@essilorusa.com>
2025-03-25 08:36:22 -0600

    luxasn has the correct credentials now and it works.

--- luxasn_map.bat
+++ luxasn_map.bat
@@ -1,3 +1,4 @@
 net use r: /DELETE /YES
 timeout /t 3 /nobreak >nul
-net use r: \\10.23.97.73\luxasn D1git@l /user:rxcppro /persistent:yes
+::net use r: \\10.23.97.73\luxasn D1git@l /user:rxcppro /persistent:yes
+net use r: \\10.23.97.73\luxasn EssiLux2024$ /user:_sa_ecoasn /persistent:yes
